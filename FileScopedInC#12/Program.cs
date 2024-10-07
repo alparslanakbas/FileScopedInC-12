0570b6dfe1accb01b5285e7bf9e5086f8ecd1c87
AlparslanTR <bybluestht@gmail.com>
2024-10-07 16:31:59 +0300

    Proje dosyası ekle.

---
+++ FileScopedInC#12/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.NetworkInformation;
+
+file class Helper
+{
+    public static string HelloWord() => "Hello Word";
+    public static string WindowsAPI()
+    {
+        string LoginName = Environment.UserName;
+        Console.WriteLine($"Oturum açan kişi: {LoginName}");
+        Console.WriteLine();
+        Console.WriteLine("Mac Adresiniz:");
+        foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+        {
+            if (nic.NetworkInterfaceType != NetworkInterfaceType.Loopback && nic.OperationalStatus == OperationalStatus.Up)
+            {
+                string MacAddress = string
+                .Join(":", nic.
+                    GetPhysicalAddress().
+                    GetAddressBytes().
+                        Select(b => b.ToString("X2"))
+                 );
+                Console.WriteLine(MacAddress);
+            }
+        }
+        return null;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(Helper.HelloWord());
+        Console.WriteLine(Helper.WindowsAPI());
+    }
+}
+
